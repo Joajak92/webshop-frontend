982452f175a571a85225326ac919cd2237e47e75
katttiger <katttiger@gmail.com>
2026-09-22 11:54:50 +0200

    cleaned up unused code and refrences

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import "./App.css";
 import LoginPage from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <LoginPage />
--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,4 +1,3 @@
-import { data } from "react-router";
 import type { LoginRequest } from "../types/LoginRequest";
 import type { LoginResponse } from "../types/LoginResponse";
 
